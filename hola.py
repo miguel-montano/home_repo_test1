b32a31db9be7d9152db741d56a150770a8afdd57
miguel montano <miguel.montano@seat.es>
2025-06-04 20:11:31 +0200

    añado un comentario del input

--- hola.py
+++ hola.py
@@ -1,3 +1,4 @@
-# This script prints "Hola, mundo" to the console.
+# pregunta al usuario su nombre y lo saluda.
 nombre = input("¿Cuál es tu nombre? ")
+# This script prints "Hola, mundo" to the console.
 print(f"Hola, {nombre}!!")
